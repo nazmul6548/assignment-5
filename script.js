693b2319a4d95bf59a0f2a83bfff936bcf886f30
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-18 16:35:39 +0600

    task

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const key = document.querySelectorAll(".kbd");
+let totalSeat =8;
+let counter = 0;
 for (const btn of key) {
     btn.addEventListener("click",() => {
+
+// console.log(btn);
+totalSeat--;
+document.getElementById('total-seat').innerText=totalSeat;
+if (totalSeat === 0) {
+    alert("All seats have been booked.")
+    
+   
+}else if(totalSeat < 0){
+    alert('Seats are overbooked!')
+    
+}
+
+
+        
         const myinnerText = btn.innerText;
+        // btn.addEventListener("click",(e) => {
+        //     totalSeat = totalSeat + 1;
+        //     document.getElementById('total-seat').innerText=totalSeat;
+        // });
+        
         const p1 =document.createElement("p")
         const p2 =document.createElement("p")
         const p3 =document.createElement("p")
@@ -12,7 +34,11 @@ for (const btn of key) {
         div.appendChild(p1);
         div.appendChild(p2);
         div.appendChild(p3);
+        
+       const convert =parseInt(p3.innerText)
+       
 
+console.log(myinnerText);
         const seatInner = document.getElementById('added-seat');
         console.log(seatInner);
         seatInner.appendChild(div);
@@ -20,6 +46,19 @@ for (const btn of key) {
         div.classList.add("justify-between")
         div.classList.add("p-5")
         
-    })
+        counter++;
+        document.getElementById('seat').innerText=counter;
+        
+    const totalcost =document.getElementById('total-cost').innerText;
+    const convertedtotalcost =parseInt(totalcost)
+    document.getElementById('total-cost').innerText=convertedtotalcost+convert;
+    //    
+
+
+
+    });
 }
 
+// for (const  of object) {
+    
+// }
